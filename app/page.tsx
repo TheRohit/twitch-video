0b2a6e596f8fe5a36b01db5150075fbada218815
TheRohit <rohitmeshram000@gmail.com>
2023-12-24 21:45:20 +0530

    chore: setup clerk webhook

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>auth worked!</div>;
+  return (
+    <div>
+      auth worked!
+      <UserButton />
+    </div>
+  );
 }
