f4197653fae1240172ce19f7fd11664aede9956b
TheRohit <rohitmeshram000@gmail.com>
2023-12-24 20:11:20 +0530

    chore: setup theme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from "react";
+import { Logo } from "./_components/logo";
 
 const layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full flex flex-col space-y-6 items-center justify-center">
+      <Logo />
+      {children}
+    </div>
+  );
 };
 
 export default layout;
